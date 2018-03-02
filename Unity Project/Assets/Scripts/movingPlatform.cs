c98765dd692009e8e6915ab511bc7f70f0bdf580
Shirmp notShrimp <quangvu151098@gmail.com>
2018-03-03 00:24:25 +0200

    @03/03/17 script update: moving platform

---
+++ Unity Project/Assets/Scripts/movingPlatform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingPlatform : MonoBehaviour {
+
+	public GameObject platform;
+
+	public float moveSpeed;
+	public int count;
+
+	public Transform currentTarget;
+	public Transform [] targets;
+
+	// Use this for initialization
+	void Start () {
+		currentTarget = targets [count];	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		platform.transform.position = 
+			Vector3.MoveTowards (platform.transform.position, currentTarget.position, moveSpeed * Time.deltaTime);
+		if (platform.transform.position == currentTarget.position) {
+			if (count == targets.Length -1) {
+				count = 0;
+			} else
+				count++;
+			currentTarget = targets [count];
+		}
+	}
+}
