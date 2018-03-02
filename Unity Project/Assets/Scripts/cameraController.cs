8424ba1492b2df2a302a7aef1895fa953452721e
Shirmp notShrimp <quangvu151098@gmail.com>
2018-03-03 00:08:57 +0200

    @02/03/18 camera controller + overview camera

---
+++ Unity Project/Assets/Scripts/cameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cameraController : MonoBehaviour {
+
+	public Camera[] cameras;
+	public Camera currentCamera;
+	public int count;
+	  
+
+	// Use this for initialization
+	void Start () {
+		currentCamera = cameras[count]; 
+		for (int i = 0; i < cameras.Length; i++) {
+			cameras [i].gameObject.SetActive (false);
+		}
+		currentCamera.gameObject.SetActive (true);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.C)) {
+			if (count == cameras.Length - 1) {
+				count = 0;
+			} else
+				count++;
+			currentCamera.gameObject.SetActive (false);
+			currentCamera = cameras [count];
+			currentCamera.gameObject.SetActive (true);
+		}	
+	}
+}
