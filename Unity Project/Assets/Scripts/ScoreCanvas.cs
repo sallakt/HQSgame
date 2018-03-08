ea38f3550f092a382277b2a9d642ad4d8f26164d
Salla Vesterinen <salla.vesterinen@gmail.com>
2018-03-08 11:33:58 +0200

    attempts added to levels for the player to see

---
+++ Unity Project/Assets/Scripts/ScoreCanvas.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCanvas : MonoBehaviour {
+
+	//finding game object from unity
+	public Text ScoreText;
+
+
+	// Use this for initialization
+	void Start () {
+		//finding UI component in Unity
+		ScoreText = GameObject.Find ("ScoreText").GetComponent<Text> ();
+		ScoreText.text = "Attempts: " + playerHealth.playerScore.ToString ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
